8ceeb842b5d2f110f7245233601f90b84de6da87
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2007-09-06 21:48:10 +0000

    ups, wrong nTorrent.sh

--- nTorrent.sh
+++ nTorrent.sh
@@ -3,10 +3,10 @@ BASE_PATH="`dirname $0`"
 
 i=""
 while (($#)); do
-	i="$i $1"
+	i="$i$1 "
 shift
 done
 
 cd $BASE_PATH
-command java -jar nTorrent.jar $1 -eq 127 || \
+command java -jar nTorrent.jar $i || \
 echo "You need java! get it at http://java.sun.com."
