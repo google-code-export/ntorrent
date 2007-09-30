1d817791a8fc48747bd4886522705b871daf97f2
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2007-09-30 11:46:33 +0000

    accepts multiple arguments properly.

--- nTorrent.sh
+++ nTorrent.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 BASE_PATH="`dirname $0`"
-
-i=""
-while (($#)); do
-	i="$i$1 "
-shift
-done
-
 cd $BASE_PATH
-command java -jar nTorrent.jar $i || \
-echo "You need java! get it at http://java.sun.com."
+java -jar nTorrent.jar "$@"
