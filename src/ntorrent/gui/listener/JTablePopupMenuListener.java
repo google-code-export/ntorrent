67c36312ff3c64961c91d93a260703c9b6df275d
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2007-09-30 16:19:23 +0000

    added skeleton for set priority on torrents.

--- src/ntorrent/gui/listener/JTablePopupMenuListener.java
+++ src/ntorrent/gui/listener/JTablePopupMenuListener.java
@@ -8,37 +8,39 @@ import ntorrent.settings.Constants.Commands;
 public abstract class JTablePopupMenuListener extends JPopupMenuListener {
 	protected int[] selectedRows;
 	
-	public JTablePopupMenuListener(Controller c, String[] menuItems) {
+	public JTablePopupMenuListener(Controller c, Object[] menuItems) {
 		super(c,menuItems);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		switch(Commands.getFromString(e.getActionCommand())){
-		case START:
-			System.out.println("Starting torrent(s)");
-			C.getMC().getTorrentPool().start(selectedRows);
-			break;
-		case STOP:
-			System.out.println("Stopping torrent(s)");
-			C.getMC().getTorrentPool().stop(selectedRows);
-			break;
-		case OPEN:
-			System.out.println("Setting torrent(s) open");
-			C.getMC().getTorrentPool().open(selectedRows);
-			break;
-		case CHECK_HASH:
-			System.out.println("Hash checking torrent(s)");
-			C.getMC().getTorrentPool().checkHash(selectedRows);
-			break;
-		case CLOSE:
-			System.out.println("Setting torrent(s) closed");
-			C.getMC().getTorrentPool().close(selectedRows);
-			break;
-		case REMOVE:
-			System.out.println("Removing torrent(s)");
-			C.getMC().getTorrentPool().erase(selectedRows);
-			break;
+		Commands cmd = Commands.getFromString(e.getActionCommand());
+		if(cmd != null)
+			switch(cmd){
+			case START:
+				System.out.println("Starting torrent(s)");
+				C.getMC().getTorrentPool().start(selectedRows);
+				break;
+			case STOP:
+				System.out.println("Stopping torrent(s)");
+				C.getMC().getTorrentPool().stop(selectedRows);
+				break;
+			case OPEN:
+				System.out.println("Setting torrent(s) open");
+				C.getMC().getTorrentPool().open(selectedRows);
+				break;
+			case CHECK_HASH:
+				System.out.println("Hash checking torrent(s)");
+				C.getMC().getTorrentPool().checkHash(selectedRows);
+				break;
+			case CLOSE:
+				System.out.println("Setting torrent(s) closed");
+				C.getMC().getTorrentPool().close(selectedRows);
+				break;
+			case REMOVE:
+				System.out.println("Removing torrent(s)");
+				C.getMC().getTorrentPool().erase(selectedRows);
+				break;
+			}
 		}
-	}
 	
 }
