583c1a8d51fd377160ad67352c774c6c7539749f
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2007-10-06 12:46:37 +0000

    backup of ntorrent before web ui integration.

---
+++ branches/ntorrent-06.10.2007/src/org/heldig/ntorrent/gui/file/InfoPanel.java
@@ -0,0 +1,64 @@
+package org.heldig.ntorrent.gui.file;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import org.heldig.ntorrent.model.TorrentInfo;
+
+
+/**
+ * @author   netbrain
+ */
+public class InfoPanel {
+	JPanel container = new JPanel(new GridLayout(0,1,10,5));
+	JScrollPane infoPanel = new JScrollPane(container);
+
+	
+	public InfoPanel(){
+		container.setOpaque(false);
+		container.setVisible(false);
+	}
+	
+	public void setInfo(TorrentInfo tf){
+		container.removeAll();
+		String[] titles = {
+				"Name", 			
+				"Directory",		
+				"Tied to file",		
+				"Size",				
+				"Files",			
+				"Message",			
+				"Priority"			
+				
+		};
+		String[] values = {
+				tf.toString(),
+				tf.getFilePath(),
+				tf.getTiedToFile(),
+				""+tf.getByteSize(),
+				""+tf.getNumFiles(),
+				tf.getMessage(),
+				""+tf.getPriority()
+				
+		};
+		for(int x = 0; x < titles.length; x++)
+			container.add(new JLabel(titles[x]+": "+values[x]));
+		container.repaint();
+		container.revalidate();
+		container.setVisible(true);
+	}
+	
+	public void hideInfo(){
+		container.setVisible(false);
+	}
+	
+	/**
+	 * @return
+	 */
+	public JScrollPane getInfoPanel() {
+		return infoPanel;
+	}
+}
---
+++ branches/ntorrent-06.10.2007/src/org/heldig/ntorrent/io/ErrorStream.java
@@ -0,0 +1,80 @@
+/**
+ *   nTorrent - A GUI client to administer a rtorrent process 
+ *   over a network connection.
+ *   
+ *   Copyright (C) 2007  Kim Eik
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.heldig.ntorrent.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.text.BadLocationException;
+
+/**
+ * @author Kim Eik
+ *
+ */
+public class ErrorStream extends OutputStream {
+    
+	JTextArea textArea = new JTextArea();
+	
+	public ErrorStream() {
+        textArea.setEditable(false);
+	}
+    
+	@Override
+	public void write(final int b) throws IOException {
+        SwingUtilities.invokeLater(new Runnable(){
+        	public void run() {
+                textArea.append(""+(char)b);
+        	}
+        });
+	}
+	
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		super.write(b, off, len);
+		setFocus();
+	}
+	
+	@Override
+	public void write(byte[] b) throws IOException {
+		super.write(b);
+		setFocus();
+	}
+	
+	private void setFocus(){
+        // Make sure the last line is always visible
+        textArea.setCaretPosition(textArea.getDocument().getLength());
+
+        //
+        while(textArea.getLineCount() > 300){
+        	try {
+				textArea.replaceRange("", textArea.getLineStartOffset(0), textArea.getLineEndOffset(0));
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+        }
+	}
+	
+	
+	public JTextArea getTextArea() {
+		return textArea;
+	}
+}
