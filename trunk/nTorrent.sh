2319c087a691d92718a97efa9277025b12d8f290
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2008-02-07 12:56:44 +0000

    adding a java plugin framework.

--- trunk/nTorrent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-BASE_PATH="`dirname $0`"
-cd $BASE_PATH
-java -jar nTorrent.jar "$@"
--- trunk/src/ntorrent/gui/profile/ProfileRequester.java
+++
@@ -1,7 +0,0 @@
-package ntorrent.gui.profile;
-
-import ntorrent.gui.profile.model.ClientProfileInterface;
-
-public interface ProfileRequester {
-	public void sendProfile(ClientProfileInterface p);
-}
