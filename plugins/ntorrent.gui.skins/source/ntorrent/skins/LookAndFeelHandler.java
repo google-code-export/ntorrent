da2e44714594b16ea54e6ed1986d53fd4b005afc
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2008-02-20 03:31:15 +0000

    cleaning up some code

--- plugins/ntorrent.gui.skins/source/ntorrent/skins/LookAndFeelHandler.java
+++ plugins/ntorrent.gui.skins/source/ntorrent/skins/LookAndFeelHandler.java
@@ -17,7 +17,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package ntorrent.gui.skins;
+package ntorrent.skins;
 
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
