0214af46eb558e03f4b048bd4a12b893465ebb90
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2008-03-14 20:50:20 +0000

    fixed svn, (faulty subclipse plugin)

---
+++ trunk/plugins/ntorrent.statusbar.throttle/source/ntorrent/statusbar/throttle/view/ThrottlePane.java
@@ -0,0 +1,57 @@
+/**
+ *   nTorrent - A GUI client to administer a rtorrent process 
+ *   over a network connection.
+ *   
+ *   Copyright (C) 2007  Kim Eik
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ntorrent.statusbar.throttle.view;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+/**
+ * @author Kim Eik
+ *
+ */
+public class ThrottlePane extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private final JSpinner up,down;
+		
+	public ThrottlePane(SpinnerNumberModel upModel, SpinnerNumberModel downModel) {
+		up = new JSpinner(upModel);
+		down = new JSpinner(downModel);
+		
+		up.setPreferredSize(new Dimension(60,20));
+		down.setPreferredSize(new Dimension(60,20));
+		
+		upModel.setMinimum(0);
+		downModel.setMinimum(0);
+		
+		add(up);
+		add(down);
+	}
+
+	public JSpinner getUp() {
+		return up;
+	}
+	
+	public JSpinner getDown() {
+		return down;
+	}
+}
