50f82435efe42f23dfce65c58130324fa4fd02fe
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2008-03-14 04:03:51 +0000

    added a simple torrentinfo tab for now.

--- plugins/ntorrent.torrentinfo/source/ntorrent/torrentinfo/TorrentInfoInstance.java
+++ plugins/ntorrent.torrentinfo/source/ntorrent/torrentinfo/TorrentInfoInstance.java
@@ -19,32 +19,103 @@
  */
 package ntorrent.torrentinfo;
 
+import java.util.Date;
+
+import javax.swing.JTabbedPane;
+import javax.swing.JTextPane;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+
+import ntorrent.io.rtorrent.Download;
+import ntorrent.locale.ResourcePool;
 import ntorrent.session.ConnectionSession;
 import ntorrent.session.SessionInstance;
+import ntorrent.torrenttable.TorrentTableInterface;
+import ntorrent.torrenttable.model.Torrent;
+import ntorrent.torrenttable.model.TorrentSelectionListener;
 
 /**
  * @author Kim Eik
  *
  */
-public class TorrentInfoInstance implements SessionInstance {
-
+public class TorrentInfoInstance implements SessionInstance,TorrentSelectionListener {
+	private final JTabbedPane tab;
+	private final JTextPane textPane = new JTextPane();
+	
+	private final Download d;
+	
+	private final static String bundle = "locale";
+	private boolean started = false;
+	
+	private final static SimpleAttributeSet BOLD = new SimpleAttributeSet();
+	static {
+		StyleConstants.setBold(BOLD, true);
+		StyleConstants.setUnderline(BOLD, true);
+	}
+	
 	public TorrentInfoInstance(ConnectionSession session) {
-		// TODO Auto-generated constructor stub
+		//get the torrenttable
+		TorrentTableInterface tc = session.getTorrentTableController();
+		
+		//tabbedpane
+		tab = session.getDisplay().getTabbedPane();
+		
+		//get the xmlrpc client for this session
+		d = session.getConnection().getDownloadClient();
+		
+		//add this as a torrent selection listener
+		tc.addTorrentSelectionListener(this);
 	}
 
 	public boolean isStarted() {
-		// TODO Auto-generated method stub
-		return false;
+		return started;
 	}
 
 	public void start() {
-		// TODO Auto-generated method stub
-		
+		started = true;
+		int preferredIndex = 0;
+		if (preferredIndex > tab.getTabCount())
+			preferredIndex = tab.getTabCount();
+
+		tab.insertTab(ResourcePool.getString("tabname", "locale", this), null, textPane, null,preferredIndex);
 	}
 
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		started = false;
+		tab.removeTabAt(tab.indexOfComponent(textPane));
 	}
+
+	public void torrentsSelected(Torrent[] tor) {
+		if(tor.length == 1){
+			textPane.setText("");
+			String hash = tor[0].getHash();
+			
+			insertText(ResourcePool.getString("name", bundle, this)+":\n", BOLD);
+			insertText("["+(d.is_open(hash) == 1 ? 
+						ResourcePool.getString("open", bundle, this) : 
+						ResourcePool.getString("closed", bundle, this))
+				+"] - "+d.get_name(hash),null);
+			insertText("\n\n"+ResourcePool.getString("hash", bundle, this)+":\n", BOLD);
+			insertText(hash, null);
+			insertText("\n\n"+ResourcePool.getString("directory", bundle, this)+":\n", BOLD);
+			insertText(d.get_directory(hash), null);
+			insertText("\n\n"+ResourcePool.getString("date-created", bundle, this)+":\n", BOLD);
+			insertText(new Date(d.get_creation_date(hash)*1000).toString(), null);
+			insertText("\n\n"+ResourcePool.getString("tied", bundle, this)+":\n", BOLD);
+			insertText(d.get_tied_to_file(hash), null);
+
+		}
+	}
+	
+	private void insertText(String text, AttributeSet set) {
+		try {
+		  textPane.getDocument().insertString(
+		      textPane.getDocument().getLength(), text, set);
+		} catch (BadLocationException e) {
+		  e.printStackTrace();
+		}
+	}	
 
 }
