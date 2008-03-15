64827cb78541dff6c311977dbed00df88f0072e0
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2008-03-15 00:17:22 +0000

    small fix

--- nTorrent.sh
+++ nTorrent.sh
@@ -4,7 +4,7 @@ NTORRENT_CONFIG="$HOME/.ntorrent/ntorrent.properties"
 
 cd $BASE_PATH
 
-if [ -f NTORRENT_CONFIG ]
+if [ -f $NTORRENT_CONFIG ]
 then
   java -Djpf.boot.config=$NTORRENT_CONFIG -jar lib/jpf-boot.jar "$@"
 else
