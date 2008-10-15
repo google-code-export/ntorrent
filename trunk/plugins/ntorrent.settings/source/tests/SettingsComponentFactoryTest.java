5e06162d0af567e9ce42d78a33e2bedb80dcbcd1
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2008-10-15 20:01:22 +0000

    added junit test, ntorrent.settings

---
+++ trunk/plugins/ntorrent.settings/source/tests/SettingsComponentFactoryTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import ntorrent.settings.view.SettingsComponentFactory;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SettingsComponentFactoryTest {
+	private final static TestModel model = new TestModel();
+	private final static TestModel controlModel = new TestModel();
+	private static SettingsComponentFactory factory;
+	
+	@BeforeClass
+	public final static void init() throws IllegalArgumentException, IllegalAccessException{
+		factory = new SettingsComponentFactory(model);
+		model.changeValues();
+	}
+	
+	@Test
+	public void testRestoreToModel() throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException{
+		factory.restoreToModel();
+		Object v = null;
+		String[] sarray = {"character","byt3","sh0rt","iint","l0ng","fl0at","duble","e","string"};
+		for(String s : sarray){
+			Object value = controlModel.getValueFromName(s);
+			v = factory.getValueFromComponent(s,value.getClass());
+			Assert.assertEquals(value,v);
+		}
+	}
+	
+	
+}
