e7ae851316426f010a92125bd6bba171fa4c6b85
kei060 <kei060@b1b4b925-f33b-0410-a4a0-7599876c56ba>
2008-10-15 20:00:52 +0000

    finished logic in ntorrent.settings plugin, and minor fixes and structural changes.

---
+++ trunk/plugins/ntorrent.settings/source/ntorrent/settings/view/SettingsComponentContainer.java
@@ -0,0 +1,53 @@
+package ntorrent.settings.view;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.HashMap;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SettingsComponentContainer{
+	private static final long serialVersionUID = 1L;
+	
+	private final JPanel container = new JPanel(new GridBagLayout());
+	private final JPanel wrapper = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	private final HashMap<String,Component> map = new HashMap<String, Component>();
+	private final GridBagConstraints constraint = new GridBagConstraints();
+	private int x = 0;
+	private int y = 0;
+	
+	public SettingsComponentContainer() {
+		this.wrapper.add(container);
+		this.constraint.fill = GridBagConstraints.HORIZONTAL;
+		this.constraint.weightx = 1;
+	}
+	
+	public JPanel getContainer() {
+		return this.wrapper;
+	}
+	
+	public void addSettingsComponent(String name, Component c){
+		addSettingsComponent(name, null, c);
+	}
+	
+	public void addSettingsComponent(String name, String label, Component c){
+		this.map.put(name, c);
+		if(label == null)
+			label = name;
+		this.constraint.gridy = this.y++;
+		this.constraint.gridx = this.x++;
+		this.constraint.gridwidth=1;
+		this.container.add(new JLabel(label+":"),this.constraint);
+		this.constraint.gridx = this.x++;
+		this.container.add(c,this.constraint);
+		this.x=0;
+	}
+	
+	public Component getComponent(String name){
+		return map.get(name);
+	}
+
+}
